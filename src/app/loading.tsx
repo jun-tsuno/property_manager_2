f0832767033f920a3345eb94365dfc3bcac8c42d
Jun <jun55tsuno@gmail.com>
2023-10-28 13:45:40 -0700

    updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
     <>
       <div className='flex flex-col items-center py-20'>
         <Spinner height={17} width={6} margin={3} />
-        <h3>Loading Page...</h3>
+        <h3>Loading...</h3>
       </div>
     </>
   );
